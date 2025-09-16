672e5a5d92d33ccdc9458a2cc5a2da373de55ff9
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-09-16 13:48:41 -0500

    Advances SimulatorOption1, now it can r and w in files officialy

--- Simulator/src/SimulatorOption1.java
+++ Simulator/src/SimulatorOption1.java
@@ -1,36 +1,51 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class SimulatorOption1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        if (args.length != 1) {
+            System.out.println("Usage: java SimulatorOption1 file");
+            return;
+        }
+
+        String fileName = args[0];
+        System.out.println(fileName);
+
+        File caseFile = new File(fileName);
+
         int TP = 0;
         int NPROC = 0;
         List<List<Integer>> TAMS = new ArrayList<>();
 
-        //Leer todas las líneas del archivo
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            if (line.startsWith("TP=")) {
-                TP = Integer.parseInt(line.split("=")[1]);
-            } else if (line.startsWith("NPROC=")) {
-                NPROC = Integer.parseInt(line.split("=")[1]);
-            } else if (line.startsWith("TAMS=")) {
-                String[] tams = line.split("=")[1].split(",");
-                for (String tam : tams) {
-                    List<Integer> mTuple = new ArrayList<>();
-                    String[] mxm = tam.split("x");
-                    mTuple.add(Integer.parseInt(mxm[0]));
-                    mTuple.add(Integer.parseInt(mxm[1]));
-                    TAMS.add(mTuple);
+        try(Scanner sc = new Scanner(caseFile)) {
+            //Leer todas las líneas del archivo
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.startsWith("TP=")) {
+                    TP = Integer.parseInt(line.split("=")[1]);
+                } else if (line.startsWith("NPROC=")) {
+                    NPROC = Integer.parseInt(line.split("=")[1]);
+                } else if (line.startsWith("TAMS=")) {
+                    String[] tams = line.split("=")[1].split(",");
+                    for (String tam : tams) {
+                        List<Integer> mTuple = new ArrayList<>();
+                        String[] mxm = tam.split("x");
+                        mTuple.add(Integer.parseInt(mxm[0]));
+                        mTuple.add(Integer.parseInt(mxm[1]));
+                        TAMS.add(mTuple);
+                    }
                 }
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
-        sc.close();
+
+        System.out.println(TP);
+        System.out.println(NPROC);
+        System.out.println(TAMS);
+
 
         //Verificar que la cantidad de tamaños de matrices sea igual a la cantidad de procesos
         if (TAMS.size() != NPROC) {
@@ -38,19 +53,35 @@ public class SimulatorOption1 {
             return;
         }
 
-        //Generar un archivo por cada proceso de acuerdo a su información respectiva
+
         for (int i = 0; i <NPROC; i++) {
             int NF = TAMS.get(i).get(0); //número de filas
             int NC = TAMS.get(i).get(1); //número de columnas
             int NR = NF*NC*3; //número de referencias (#enteros por matriz x 3 matrices)
             int NP = (int) Math.ceil(NR*4.0/TP); //número de páginas virtuales
-            String fileName = "proc"+i+".txt";
-            try (PrintWriter pw = new PrintWriter(new FileWriter(fileName))) {
-                pw.println("TP=" + TP);
-                pw.println("NF=" + NF);
-                pw.println("NC=" + NC);
-                pw.println("NR=" + NR);
-                pw.println("NP=" + NP);
+
+            //Generar un archivo por cada proceso de acuerdo a su información respectiva
+            fileName = "proc"+i+".txt";
+            try {
+                File newFile = new File(fileName);
+                if (newFile.createNewFile()) {
+                    System.out.println("File created: " + newFile.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                throw new RuntimeException(e);
+            }
+
+            // Escribir la información de la memoria virtual asignada para el proceso
+            try (FileWriter writer = new FileWriter(fileName)) {
+                writer.write("TP=" + TP +"\n");
+                writer.write("NF=" + NF +"\n");
+                writer.write("NC=" + NC +"\n");
+                writer.write("NR=" + NR +"\n");
+                writer.write("NP=" + NP +"\n");
+                System.out.println("Successfully wrote to the file.");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
